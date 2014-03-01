7c180e0f33c9bc9b9b36044ee9e3e0f5932f9c74
Johan <joacub@gmail.com>
2014-03-01 14:52:48 +0100

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,9 @@
 <?php
 return array(
     'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
         'template_map' => include __DIR__  .'/../template_map.php',
     ),
     'controllers' => array(
